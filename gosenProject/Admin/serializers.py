80e26d600135db3001888ad131be984b16616c81
Cova14 <arty1498@hotmail.com>
2019-07-05 11:37:18 -0500

    Showing whole User instead of the Id

--- gosenProject/Admin/serializers.py
+++ gosenProject/Admin/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Admin
+from accounts.serializers import UserSerializer
 
 class AdminSerializer(serializers.ModelSerializer):
-
+    user = UserSerializer(many=False, required=True)
     class Meta:
         model = Admin
         fields = '__all__'
