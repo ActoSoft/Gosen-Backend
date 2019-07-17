666854ced533d3731c2ad7b083540342596d37b4
Martin Melo <nitram-210397@hotmail.com>
2019-07-16 21:28:07 -0500

    Updated contact_date start from DateTimeField for only DateField

--- gosenProject/employee/models.py
+++ gosenProject/employee/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from accounts.models import Profile
 from django.contrib.auth.models import User
 
-PAYMENT_TIPE_LIST = (
+PAYMENT_TYPE_LIST = (
     ('semanal', 'Semanal'),
     ('quincenal', 'Quincenal'),
     ('mensual', 'Mensual'),
@@ -10,14 +10,14 @@ PAYMENT_TIPE_LIST = (
 
 
 class Employee(Profile):
-    contract_date_start = models.DateTimeField(blank=True, null=True)
+    contract_date_start = models.DateField(blank=True, null=True)
     contracted_by = models.OneToOneField(User, on_delete=models.CASCADE, related_name='contractor', blank=True, null=True)
     vigency = models.CharField(max_length=150, blank=True, null=True)
     salary = models.CharField(max_length=150, blank=True, null=True)
-    payment_type = models.CharField(choices=PAYMENT_TIPE_LIST, max_length=100, default='Quincenal')
+    payment_type = models.CharField(choices=PAYMENT_TYPE_LIST, max_length=100, default='Quincenal')
     active = models.BooleanField(default=True)
     fired = models.BooleanField(default=False)
-    fired_date = models.DateTimeField(blank=True, null=True)
+    fired_date = models.DateField(blank=True, null=True)
     fired_by = models.OneToOneField(User, on_delete=models.CASCADE, related_name='firer', blank=True, null=True)
     available = models.BooleanField(default=True)
 
