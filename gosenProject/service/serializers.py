4ab5899db5ddedc207ae6bee1c13355f448b5b0a
Martin Melo Godinez <martin.melo.dev.97@gmail.com>
2019-11-22 01:33:03 -0600

    Added nested relation on services serializer with works

--- gosenProject/service/serializers.py
+++ gosenProject/service/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Service
+from works.pre_serializers import WorkSerializerOnlyRead
 
 
 class ServiceSerializer(serializers.ModelSerializer):
-
+    works = WorkSerializerOnlyRead(required=False, many=True)
     class Meta:
         model = Service
         fields = '__all__'
