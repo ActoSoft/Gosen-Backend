ffccffce3d9d5796ea16fc8ff26551f31a32d02c
Martin Melo Godinez <martin.melo.dev.97@gmail.com>
2019-11-22 00:44:04 -0600

    Change default value on payment type on services model

--- gosenProject/service/models.py
+++ gosenProject/service/models.py
@@ -17,7 +17,7 @@ class Service(models.Model):
     name = models.CharField(max_length=150, blank=False)
     description = models.CharField(max_length=150, blank=True, null=True)
     cost = models.DecimalField(max_digits=7, decimal_places=2)
-    payment_type = models.CharField(choices=PAYMENT_TIPE_LIST, max_length=100, default='Quincenal')
+    payment_type = models.CharField(choices=PAYMENT_TIPE_LIST, max_length=100, default='quincenal')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     deleted = models.DateTimeField(blank=True, null=True)
