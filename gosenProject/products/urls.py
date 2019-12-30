56a3680d76d17176007673c7bf1b8faebb3264c9
Martin Melo Godinez <martin.melo.dev.97@gmail.com>
2019-12-30 10:40:05 -0600

    Create APIView to handle create product into a stock

--- gosenProject/products/urls.py
+++ gosenProject/products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, UploadImage, DeleteImage
+from .views import ProductViewSet, UploadImage, DeleteImage, SaveStock
 
 router = DefaultRouter()
 
@@ -9,6 +9,7 @@ router.register('', ProductViewSet)
 
 urlpatterns = [
     path('upload_image/', UploadImage.as_view()),
-    path('delete_image/', DeleteImage.as_view())
+    path('delete_image/', DeleteImage.as_view()),
+    path('save_stock/', SaveStock.as_view())
 ]
 urlpatterns += router.urls
--- gosenProject/products/views.py
+++ gosenProject/products/views.py
@@ -1,4 +1,5 @@
-from .models import Product, ProductImage
+from .models import Product, ProductImage, ProductStock
+from stocks.models import Stock
 from .serializers.common import ProductListSerializer, ProductDetailSerializer, ProductCreateSerializer
 import datetime
 from rest_framework import viewsets, status, parsers, views
@@ -66,3 +67,28 @@ class DeleteImage(views.APIView):
             print(e)
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
+class SaveStock(views.APIView):
+    def post(self, request):
+        try:
+            if not request.data.get('stock'):
+                return Response({"message": "Not stock in request"}, status=status.HTTP_400_BAD_REQUEST)
+            elif not request.data.get('product'):
+                return Response({"message": "Not product in request"}, status=status.HTTP_400_BAD_REQUEST)
+            elif not request.data.get('qty') or request.data['qty'] < 1:
+                return Response({"message": "Not qty in request"}, status=status.HTTP_400_BAD_REQUEST)
+            stock = Stock.objects.get(id=request.data['stock'])
+            product = Product.objects.get(id=request.data['product'])
+            product_stock = ProductStock.objects.create(
+                product=product,
+                stock=stock,
+                qty=request.data['qty']
+            )
+            if product_stock is not None:
+                product_serializer = ProductDetailSerializer(product)
+                return Response(product_serializer.data)
+            else:
+                return Response({"message": "Algo falló al guardar"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(e)
+            return Response({"message": "Algo falló al guardar"}, status=status.HTTP_400_BAD_REQUEST)
