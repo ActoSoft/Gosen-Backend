6eb9bf4a4d72f7ba17605bace693afb46ebd26f6
Martin Melo Godinez <martin.melo.dev.97@gmail.com>
2020-01-22 14:52:55 -0600

    Added APIView to handle create transactions related to a work and also update the totals values

--- gosenProject/financials/urls.py
+++ gosenProject/financials/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import TransactionViewSet
+from .views import TransactionViewSet, AddTransactionRelatedToWork
 
 router = DefaultRouter()
 router.register('transactions', TransactionViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('create_transaction/', AddTransactionRelatedToWork.as_view())
+]
+
+urlpatterns += router.urls
--- gosenProject/financials/views.py
+++ gosenProject/financials/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Financial, Transaction
+from works.models import Work
+from works.serializers.common import WorkDetailSerializer
 from .serializers.common import FinancialSerializer, TransactionListSerializer, TransactionDetailSerializer
 import datetime
 from rest_framework import viewsets, status, views
@@ -27,3 +29,32 @@ class TransactionViewSet(viewsets.ModelViewSet):
         transaction.deleted = now
         transaction.save()
         return Response({'message': 'ok'})
+
+
+class AddTransactionRelatedToWork(views.APIView):
+    def post(self, request):
+        try:
+            work = Work.objects.get(id=request.data['work'])
+            if request.data.get('amount') and request.data.get('concept'):
+                new_transaction = Transaction.objects.create(
+                    amount=request.data['amount'],
+                    concept=request.data['concept'],
+                    type=request.data['type'],
+                    work=work
+                )
+                if new_transaction is not None:
+                    work.payed += request.data['amount']
+                    work.to_pay = work.total - work.payed
+                    work.save()
+                    work_serialized = WorkDetailSerializer(work)
+                    return Response(work_serialized.data)
+                else:
+                    return Response({'message': 'Algo falló al crear el pago'},
+                                    status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response({'message': 'Los datos enviados son inváldiso'},
+                                status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(e)
+            return Response({'message': 'Algo falló al crear el pago'},
+                            status=status.HTTP_400_BAD_REQUEST)
